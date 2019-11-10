dd730877194ab795540f3ca6ca512fabacf68c30
Nastusha <55599792+bondnastia@users.noreply.github.com>
2019-11-10 21:56:36 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+$('.spoilerButton').on('click', function(){
+    $('.spoilerButton').hide();
+    $('.spoiler').fadeIn(200);
+})
+
+$('#hide').click(function(){
+    $('.spoiler').fadeOut()
+})
+// end
+
+$('.spol').hide();
+$('.spolButton').on('click', function(){
+    $('.spolButton').hide();
+    $('.spol').fadeIn(200);
+})
